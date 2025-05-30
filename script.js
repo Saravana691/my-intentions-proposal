4b00f720e86d91363e2428b69cf52cf029ad34e9
Saravana691 <saravanasmart691@gmail.com>
2025-05-30 22:54:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 function showLove() {
-  const surprise = document.getElementById("surprise");
-  surprise.classList.remove("hidden");
-
-  // Trigger pop animation by adding 'pop' class
-  surprise.classList.add("pop");
-
-  // Optional: disable the button after click so they can't spam it
-  document.querySelector("button").disabled = true;
+  const surprise = document.getElementById('surprise');
+  surprise.classList.remove('hidden');
 }
 
