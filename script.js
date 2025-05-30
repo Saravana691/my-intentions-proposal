debf5d23b0c1c7d55cd12fbc39b9c87e0a39d41c
Saravana691 <saravanasmart691@gmail.com>
2025-05-30 23:00:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function showLove() {
   const surprise = document.getElementById('surprise');
   surprise.classList.remove('hidden');
-}
 
+  // Triggering the show animation
+  setTimeout(() => {
+    surprise.classList.add('show');
+  }, 50);
+}
